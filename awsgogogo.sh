4676a1aa32fe1b29d202d1d9039140217af704c9
Weijialiu <weijialiu@hiretual.com>
2021-04-27 06:56:27 +0000

    fix some bugs,to adpt ubuntu.

--- awsgogogo.sh
+++ awsgogogo.sh
@@ -6,12 +6,12 @@
 # Time 2021.04.20
 
 # Read config
-durationSeconds=`sed '/^duration-seconds=/!d;s/.*=//' config`
-serialNumber=`sed '/^serial-number=/!d;s/.*=//' config`
-credentialsPath=`sed '/^credentials-path=/!d;s/.*=//' config`
+durationSeconds=`sed '/^duration-seconds=/!d;s/.*=//' /home/weijialiu/code/AWS_MFA_Configurator/config`
+serialNumber=`sed '/^serial-number=/!d;s/.*=//' /home/weijialiu/code/AWS_MFA_Configurator/config`
+credentialsPath=$(sed '/^credentials-path=/!d;s/.*=//' /home/weijialiu/code/AWS_MFA_Configurator/config)
 
 cp $credentialsPath $credentialsPath.backup
-echo "\033[32m"
+sudo echo "\033[32m"
 echo "$credentialsPath file has been backed up!"
 
 echo
@@ -23,14 +23,14 @@ echo '============================================================='
 echo 
 
 # input token-code
-echo "\033[31m"
+sudo echo "\033[31m"
 echo "(The most fu**king tedious step) Please input token-code:"
 read -p "Fu*king write here: " token
-echo "\033[32m"
+sudo echo "\033[32m"
 echo
 
 # print request command
-echo "\033[32m"
+sudo echo "\033[32m"
 echo '======================== Your Request ======================='
 echo  Your request:
 echo $ aws sts get-session-token --duration-seconds $durationSeconds --serial-number $serialNumber --token-code $token
@@ -55,29 +55,34 @@ else
     result_SecretAccessKey=`echo $result | cut -d\" -f10 `
     result_SessionToken=`echo $result | cut -d\" -f14 `
 
-    echo result_AccessKeyId = $result_AccessKeyId
-    echo result_SecretAccessKey = $result_SecretAccessKey
-    echo result_SessionToken = $result_SessionToken
+    echo result_AccessKeyId=$result_AccessKeyId
+    echo result_SecretAccessKey=$result_SecretAccessKey
+    echo result_SessionToken=$result_SessionToken
 
     # modify file
     echo
-    echo 'Delete old configuration...'
+    echo 'Prepare to delete old configuration...'
 
     line=$(sed -n -e '/mfa/=' $credentialsPath)
     # line=`grep -n "[mfa]" $credentialsPath | cut -d ":" -f 1`
-    # echo $line
-    lastLine=$[line+3]
-    # echo $lastLine
-    echo
-
+    
+    if [ $line ]
+    then
+        echo 'find the old configuration begin line:'$line
+        lastLine=$[line+3]
+        echo 'the last line:'$lastLine
+        echo
+        sed -i "$line,$lastLine d" $credentialsPath
+    else
+        echo 'Do not find the old configuration'
+    fi
     echo 'Add new configuration...'
-    sed -i "" "$line,$lastLine d" $credentialsPath
-
+    
     # add line
     echo '[mfa]' >> $credentialsPath
-    echo 'aws_access_key_id =' $result_AccessKeyId >> $credentialsPath
-    echo 'aws_secret_access_key =' $result_SecretAccessKey >> $credentialsPath
-    echo 'aws_session_token =' $result_SessionToken >> $credentialsPath
+    echo 'aws_access_key_id='$result_AccessKeyId >> $credentialsPath
+    echo 'aws_secret_access_key='$result_SecretAccessKey >> $credentialsPath
+    echo 'aws_session_token='$result_SessionToken >> $credentialsPath
     echo 
 
     echo 'Dnoe !!! You can really dance!'
